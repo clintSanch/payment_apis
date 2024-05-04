4a248e93dfa1ee70c8b13462b82566d7933b2299
clintSanch <clintonodira@gmail.com>
2024-05-04 18:03:14 +0300

    integrating sentry in the application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,49 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withDebugTracing } from '@angular/router';
+import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, PLATFORM_ID } from '@angular/core';
+import { Router, provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import * as Sentry from '@sentry/angular-ivy';
+import { isPlatformServer } from '@angular/common';
 
+function provideSentryProviders(platformId: object): unknown[] {
+  if (isPlatformServer(platformId)) {
+    return [];
+  }
+  return [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        logErrors: true,
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => { },
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ];
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withDebugTracing()), 
+    provideRouter(routes, withDebugTracing()),
     provideClientHydration(
       withHttpTransferCacheOptions({ //configure this cache by using withHttpTransferCacheOptions when providing hydration.
-      includePostRequests: true, 
-    })), 
-    provideHttpClient(withFetch())]
+        includePostRequests: true,
+      })),
+    provideHttpClient(withFetch()),
+    {
+      provide: 'sentryProviders',
+      useFactory: provideSentryProviders,
+      deps: [PLATFORM_ID]
+    }
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import * as Sentry from '@sentry/angular-ivy';
+
+Sentry.init({
+  dsn: "https://10e4f4be2f99a71f5c4625f57c78a0ca@o4507198588649472.ingest.de.sentry.io/4507198614339664",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+enableProdMode();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
